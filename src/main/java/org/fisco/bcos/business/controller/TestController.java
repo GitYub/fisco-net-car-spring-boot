698ac2bb6648f00c27c841eb91ab89c24a741ddb
GitYub <2256676zx?>
2019-07-24 19:13:49 +0800

    test

--- src/main/java/org/fisco/bcos/business/controller/TestController.java
+++ src/main/java/org/fisco/bcos/business/controller/TestController.java
@@ -2,24 +2,35 @@ package org.fisco.bcos.business.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.fisco.bcos.business.util.JsonData;
+import org.fisco.bcos.web3j.protocol.Web3j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertTrue;
 
 @RestController
 @RequestMapping("/test")
 @Slf4j
 public class TestController {
 
-    @Autowired
-    TestController() {
+    private Web3j web3j;
 
+    @Autowired
+    TestController(Web3j web3j) {
+        this.web3j = web3j;
     }
 
     @GetMapping("/string")
-    public JsonData test() {
+    public JsonData test() throws IOException {
         log.info("test");
+        BigInteger blockNumber = web3j.getBlockNumber().send().getBlockNumber();
+        log.info("blockNumber is {}", blockNumber);
+        assertTrue(blockNumber.compareTo(new BigInteger("0")) >= 0);
         return JsonData.success("123");
     }
 
