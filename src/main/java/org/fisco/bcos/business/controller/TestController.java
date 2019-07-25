f1ba1438ef2e05e38a437020915fc49d1d83b587
GitYub <2256676zx?>
2019-07-25 10:17:29 +0800

    test deploy assets

--- src/main/java/org/fisco/bcos/business/controller/TestController.java
+++ src/main/java/org/fisco/bcos/business/controller/TestController.java
@@ -3,9 +3,13 @@ package org.fisco.bcos.business.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.fisco.bcos.BAC001;
 import org.fisco.bcos.business.util.JsonData;
+import org.fisco.bcos.web3j.crypto.Credentials;
 import org.fisco.bcos.web3j.protocol.Web3j;
+import org.fisco.bcos.web3j.tx.gas.ContractGasProvider;
+import org.fisco.bcos.web3j.tx.gas.StaticGasProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,10 +48,31 @@ public class TestController {
 
         log.info("version is {}", version);
 
-        BigInteger minAmount = BigInteger.ONE;
-        BigInteger maxAmount = BigInteger.TEN;
-        BAC001 bac001 = BAC001.deploy(web3j, );
-        BigInteger bigInteger = bac001.totalAmount().send().abs();
+        return JsonData.success("123");
+    }
+
+    @PostMapping("/deploy")
+    public JsonData deploy() throws Exception {
+
+        BigInteger gasPrice = new BigInteger("1");
+        BigInteger gasLimit = new BigInteger("210000000");
+
+        ContractGasProvider contractGasProvider = new StaticGasProvider(gasPrice, gasLimit);
+        Credentials credentials = Credentials.create("123456");
+
+        String description = "fisco net car integration";
+        String shortName = "integration";
+        BigInteger minAmount = BigInteger.valueOf(1);
+        BigInteger maxAmount = BigInteger.valueOf(10000000);
+
+        BAC001 bac001 = BAC001.deploy(web3j, credentials, contractGasProvider,
+                description, shortName, minAmount, maxAmount).send();
+
+        BigInteger totalAmount = bac001.totalAmount().send().abs();
+        String contractAddress = bac001.getContractAddress();
+
+        log.info("total amount is : {}", totalAmount);
+        log.info("contract address is : {}", contractAddress);
 
         return JsonData.success("123");
     }
